80cc0246feb16f083815f22a82fbd0cf4f8b7b98
lselbeck <lselbeck@gmail.com>
2015-03-16 13:59:49 -0700

    working on iNode

--- Inode.java
+++ Inode.java
@@ -9,13 +9,13 @@ public class Inode {
       private final static int iNodeSize = 32;       // fix to 32 bytes
       private final static int directSize = 11;      // # direct pointers
 
-      public int length;               // file size in bytes
-      public short count;              // # file-table entries pointing to this
-      public short flag;               // 0 = unused, 1 = used, ...
-      public short direct[] = new short[directSize]; // direct pointers
-      public short indirect;                         // a indirect pointer
+      public int length; //4 bytes     // file size in bytes
+      public short count;//2 bytes     // # file-table entries pointing to this
+      public short flag; //2 bytes     // 0 = unused, 1 = used, ...
+      public short direct[] = new short[directSize];//22 bytes; direct pointers
+      public short indirect; //2 bytes              // a indirect pointer
 
-      Inode( ) {                                     // a default constructor
+      Inode( ) {                                    // a default constructor
          length = 0;
          count = 0;
          flag = 1;
@@ -25,7 +25,17 @@ public class Inode {
       }
 
       Inode( short iNumber ) {         // retrieving inode from disk
-         // design it by yourself.
+         byte buffer[] = new byte[32];
+         SysLib.int2bytes(length, buffer, 0);
+         SysLib.short2bytes(count, buffer, 4);
+         SysLib.short2bytes(flag, buffer, 6);
+         for ( int i = 0; i < directSize; i++ )
+         {
+            SysLib.short2bytes(direct[i], buffer, 8 + i*2);
+         }
+         SysLib.short2bytes(indirect, buffer, 30);
+
+         //write to correct space on disk
       }
 
       int toDisk( short iNumber ) {    // save to disk as the i-th inode
