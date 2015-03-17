7a5be58df9d5a8b7c3f24a8a19e67cff9bdb0f67
Dvaness <dallasvaness@gmail.com>
2015-03-17 09:35:56 -0700

    Finished Directory and FileTableEntry

--- FileTableEntry.java
+++ FileTableEntry.java
@@ -6,12 +6,22 @@
 //The file system maintains the file (structure) table shared among all user
 //threads
 
-public class FileTableEntry {          // Each table entry should have
+
+
+public class FileTableEntry             // Each table entry should have
+{
 	public int seekPtr;                 //    a file seek pointer
 	public final Inode inode;           //    a reference to its inode
 	public final short iNumber;         //    this inode number
 	public int count;                   //    # threads sharing this entry
 	public final String mode;           //    "r", "w", "w+", or "a"
+
+	//modes
+	public static final short READONLY = 0;
+	public static final short WRITEONLY = 1;
+	public static final short READWRITE = 2;
+	public static final short APPEND = 3;
+
 	public FileTableEntry ( Inode i, short inumber, String m ) {
 		seekPtr = 0;             // the seek pointer is set to the file top
 		inode = i;
@@ -20,5 +30,32 @@ public class FileTableEntry {          // Each table entry should have
 		mode = m;                // once access mode is set, it never changes
 		if ( mode.compareTo( "a" ) == 0 ) // if mode is append,
 		seekPtr = inode.length;        // seekPtr points to the end of file
+	}
+
+	public static short getMode(String mode)
+	{
+		mode = mode.toLowerCase();
+
+		if(mode.compareTo("r") == 0)
+		{
+			return READONLY;
+		}
+
+		if(mode.compareTo("w") == 0)
+		{
+			return WRITEONLY;
+		}
+
+		if(mode.compareTo("w+") == 0)
+		{
+			return READWRITE;
+		}
+
+		if(mode.compareTo("a") == 0)
+		{
+			return APPEND;
+		}
+
+		return -1;
 	}
 }
